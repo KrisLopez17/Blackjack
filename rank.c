146e531f22536706e14872e9b42cadb1901377a3
Kristen <netsirk2005@gmail.com>
2026-09-04 12:48:26 -0500

    Create rank.c

---
+++ rank.c
@@ -0,0 +1,22 @@
+/**
+*@brief generate rank
+*@author Kristen Stough
+*/
+
+#include "deck.h"
+
+typedef enum {
+    TWO = 2,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+} Rank;
