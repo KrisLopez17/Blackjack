efb5cd9c1c0d34cacb85211fbddf1342a60ad2e9
Kristen <netsirk2005@gmail.com>
2026-09-04 12:03:18 -0500

    Step 1: Create a Card
    Create a process for generating a card from a standard playing deck.

--- Blackjack.c
+++ Blackjack.c
@@ -8,3 +8,36 @@
 int main(){
     return 0
 }
+
+//step 1: create a card
+
+typedef enum {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES,
+} Suit;
+
+typedef enum {
+    TWO = 2,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+} Rank;
+
+typedef struct {
+    Rank rank;
+    Suit suit;
+} Card;
+
+int card_value(Card card);
+void print_card(Card card);
